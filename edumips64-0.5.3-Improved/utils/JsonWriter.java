ea5101b26990db53b2e3c99bde6a89eed2a13d17
Sam Cleland <edward.cleland@mail.mcgill.ca>
2019-03-01 18:21:26 -0500

    work in progress

--- edumips64-0.5.3-Improved/utils/JsonWriter.java
+++ edumips64-0.5.3-Improved/utils/JsonWriter.java
@@ -1,7 +1,7 @@
 package utils;
 
 import edumips64.core.cache.CacheManager;
-import edumips64.core.cache.ICache.CacheLayer;
+import edumips64.core.cache.cacheLayer.CacheLayer;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONArray;
 import java.util.List;
@@ -11,11 +11,16 @@ public class JsonWriter {
     public void write() {
         JSONObject obj = new JSONObject();
         CacheManager cm = CacheManager.getInstance();
-        CacheLayer[] layers = (CacheLayer[]) cm.getCacheLayers();
+        CacheLayer[] layers =  cm.getCacheLayers();
         obj.put("number_of_caches", layers.length);
         JSONArray blockSizes = new JSONArray();
+        JSONArray strategies = new JSONArray();
         for(CacheLayer cl : layers) {
-            blockSizes.add(cl)
+            blockSizes.add(cl.getBlockSize());
+            strategies.add(cl.getWriteStrategy().name());
         }
+
+        obj.put("block_sizes", blockSizes);
+        obj.put("write_strategies", strategies);
     }
 }
