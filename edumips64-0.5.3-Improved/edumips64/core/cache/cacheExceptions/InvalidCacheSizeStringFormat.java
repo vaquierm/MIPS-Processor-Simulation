c896fb11f7f679d2527655de2673bf00f5d8dd43
Robert <robert.attard@mail.mcgill.ca>
2019-03-03 13:31:47 -0500

    Feature/setup cache manager (#30)
    * fixed instance not being set
    
    * loaded json, and sarted validation
    
    need to do configuration next and fnish validation of inputs
    
    * simplified power of 2 check
    
    * pretty much done configuration for DM
    
    need to do for other types and fix the error handling
    
    * mostly done with configs
    
    * added some json
    
    * Bugs fixed for constructors of cache layers
    
    * Changed 'way' filed to Mapping scheme
    
    * Added JSON config file with no caches, modified caches to only have DIRECT mapping
    
    * clear cache configuration button added to cache dropdown
    
    * added error popup on exception from loading
    
    * Added dialog boxes to confirm load and reset
    
    * squashed commits, done config
    
    Merge pull request #16 from vaquierm/feature/integration-with-Memory
    
    Feature/integration with memory
    Merge pull request #17 from vaquierm/feature/associative-cache
    
    Feature/associative cache
    pretty much done configuration for DM
    
    need to do for other types and fix the error handling
    Merge branch 'master' of https://github.com/Vaquierm/MIPS-Processor-Simulation into feature/setupCacheManager
    
    Merge pull request #19 from vaquierm/Write-through
    
    Write through
    mostly done with configs
    
    added some json
    
    wtf why am i getting merge conflicts in shit i didnt touch
    
    Bugs fixed for constructors of cache layers
    
    Changed 'way' filed to Mapping scheme
    
    Added JSON config file with no caches, modified caches to only have DIRECT mapping
    
    clear cache configuration button added to cache dropdown
    
    
    added error popup on exception from loading
    
    
    Added dialog boxes to confirm load and reset
    
    
    show button when halted (i have no idea how to make it halted)

--- edumips64-0.5.3-Improved/edumips64/core/cache/cacheExceptions/InvalidCacheSizeStringFormat.java
+++
@@ -1,5 +0,0 @@
-package edumips64.core.cache.cacheExceptions;
-
-public class InvalidCacheSizeStringFormat extends Exception {
-    public InvalidCacheSizeStringFormat() { super("String must be of format XXXB, XXXKB, XXXMB, or XXXGB"); }
-}
